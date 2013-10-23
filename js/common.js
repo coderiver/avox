13264ba9d8c2db58298de7da2c6de2aca7ff0280
akella <akella.a@gmail.com>
2013-10-23 15:56:46 +0300

    gogo

---
+++ js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	//showing popup
+	$('.showme').click(function (e) {
+		$('.overlay,.popup').addClass('is-visible');
+		return false;
+	});
+
+
+	//chosen stuff
+	var config = {
+      '.chosen-select'           : {},
+      '.chosen-select-deselect'  : {allow_single_deselect:true},
+      '.chosen-select-no-single' : {disable_search_threshold:10},
+      '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
+      '.chosen-select-width'     : {width:"95%"}
+    }
+    for (var selector in config) {
+      $(selector).chosen(config[selector]);
+    }
+});
