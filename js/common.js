7b5be4bf83e5864fb74d7216484e46ee37162087
akella <akella.a@gmail.com>
2013-10-31 14:06:26 +0200

    all the fixes

--- js/common.js
+++ js/common.js
@@ -1,45 +1,77 @@
 $(document).ready(function() {
 
-  //chosen stuff
-  var config = {
-      '.chosen-select'           : {stay_open:true},
-      '.chosen-select-deselect'  : {allow_single_deselect:true},
-      '.chosen-select-no-single' : {disable_search_threshold:10},
-      '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
-      '.chosen-select-width'     : {width:"95%"}
-    }
-    for (var selector in config) {
-      $(selector).chosen(config[selector]);
-    }
-    function add_new_row() {
-      if ($(".js-table-row").length > 0) {
-        $(".group__add").bind("click",function(){
-          var last_tr = $(this).parent().parent().find(".table tbody tr").last();
-          var html = $(this).parent().parent().find(".js-table-row tr").html();
-          last_tr.after("<tr>"+html+"</tr>");
-          last_tr.next().find(".js-select").chosen();
-          return false;
-        });
-      }
-    }
-    add_new_row();
+	//chosen stuff
+	var config = {
+			'.chosen-select'           : {"disable_search_threshold": 10},
+			'.chosen-select-deselect'  : {allow_single_deselect:true},
+			'.chosen-select-no-single' : {disable_search_threshold:10},
+			'.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
+			'.chosen-select-width'     : {width:"95%"}
+		}
+		for (var selector in config) {
+			$(selector).chosen(config[selector]).change(function(){ $(this).trigger('chosen:updated') });
+		}
+		function add_new_row() {
+			if ($(".js-table-row").length > 0) {
+				$(".group__add:not(.group__addnote)").bind("click",function(){
+					var wheretoprepend = $(this).parent().parent().find(".table tbody tr:first-child");
+					var html = $(this).parent().parent().find(".js-table-row tr").html();
+					wheretoprepend.before('<tr>'+html+'</tr>');
+					newtr = wheretoprepend.prev();
+						newtr.find(".js-select").chosen().change(function(){ $(this).trigger('chosen:updated') });
+						newtr.find("select[multiple]").multiselect({
+							selectedList: 4,
+							noneSelectedText: "Select an Option",
+							header: ""
+						}).multiselectfilter();
+					return false;
+				});
+			}
+		}
+		add_new_row();
+
+		$('.table').on('click', '.icon-delete', function(){
+			$(this).closest('tr').remove();
+		});
+		$('.group').on('change', 'h2 input[type=checkbox]', function(){
+			$(this).closest('.group').toggleClass('is-inactive-group');
+
+		});
 
 
-    $('.icon-delete').click(function (e) {
-      var strconfirm = confirm("Are you sure you want to delete?");
-      if (strconfirm == true) {
-            $(this).closest('tr').remove();
-        }
-    });
-
-    $(".js-multi-select").multiselect({
-        selectedList: 4,
-        noneSelectedText: "Select an Option",
-        header: ""
-    }).multiselectfilter();
+		$(".js-select-multi").multiselect({
+				selectedList: 4,
+				noneSelectedText: "Select an Option",
+				header: "",
+   		 	open: function () {$(this).multiselect("widget").find("input[type='search']:first").focus();}
+ 
+		}).multiselectfilter();
 
 
-    $('.clearselect').click(function (e) {
-     $(this).siblings('select').multiselect("uncheckAll");
-    });
+		// hiding on blur, needs class, is-shown
+		mouseOverActiveElement = false;
+		$('.group__addnote').click(function () {
+			$(this).children('.addnote').addClass('is-dropped').show();
+		});
+		//mouseOverActiveElement = false;
+		$('body').on('mouseenter', '.is-activeelement', function(){
+			mouseOverActiveElement = true; 
+		});
+		$('body').on('mouseleave', '.is-activeelement', function(){
+			mouseOverActiveElement = false; 
+		});
+		$("html").click(function(){ 
+		    if (!mouseOverActiveElement) {
+		    	$('.is-dropped').hide();
+		        console.log('clicked outside active element');
+		    }
+		});
+		$(".addnote__submit").click(function (e) {
+			$('.addnote').hide();
+			newtext = $('.addnote').find('textarea').val();
+			newhtml = '<tr><td>John Doe</td><td>1970</td><td>'+newtext+'</td><td><a href="#">filename</a></td><td class="td_icon"><div class="icon-delete"></div></td></tr>';
+			var wheretoprepend = $('.table tbody tr:first-child');
+			wheretoprepend.before(newhtml);
+		});
+
 });
