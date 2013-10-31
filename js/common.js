6a0c6622445e81d3d2aa8ebda0054603ba54f026
akella <akella.a@gmail.com>
2013-10-31 14:07:33 +0200

    15 threshold

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	//chosen stuff
 	var config = {
-			'.chosen-select'           : {"disable_search_threshold": 10},
+			'.chosen-select'           : {"disable_search_threshold": 15},
 			'.chosen-select-deselect'  : {allow_single_deselect:true},
 			'.chosen-select-no-single' : {disable_search_threshold:10},
 			'.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
